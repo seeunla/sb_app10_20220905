48c2f0e632301416a092e731912c38066d69d49b
seeunla <107592967+seeunla@users.noreply.github.com>
2022-09-05 15:06:39 +0900

    추가 세팅

---
+++ src/main/java/com/ll/exam/app10/app/fileUpload/entity/Genfile.java
@@ -0,0 +1,32 @@
+package com.ll.exam.app10.app.fileUpload.entity;
+
+import com.ll.exam.app10.app.base.entity.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+public class Genfile extends BaseEntity {
+    private String relTypeCode;
+    private int relId;
+    private String typeCode;
+    private String type2Code;
+    private String fileExtTypeCode;
+    private String fileExtType2Code;
+    private int fileSize;
+    private int fileNo;
+    private String fileExt;
+    private String fileDir;
+    private String originFileName;
+}
