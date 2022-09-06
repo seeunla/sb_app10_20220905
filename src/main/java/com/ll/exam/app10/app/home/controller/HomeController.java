43b5616503a6460ca9063ee99a2b95ff68a5d6be
seeunla <107592967+seeunla@users.noreply.github.com>
2022-09-06 14:56:08 +0900

    회원가입 폼

--- src/main/java/com/ll/exam/app10/app/home/controller/HomeController.java
+++ src/main/java/com/ll/exam/app10/app/home/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.ll.exam.app10.app.home.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,4 +17,5 @@ public class HomeController {
     public String upload() {
         return "home/test/upload";
     }
+
 }
