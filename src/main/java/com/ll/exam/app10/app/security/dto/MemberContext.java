e3b178b9f35d468880393d558bdcf3afe60a18cb
seeunla <107592967+seeunla@users.noreply.github.com>
2022-09-19 14:09:18 +0900

    이미지를 변경하고서 캐시에 상관없이 이미지가 자동으로 업데이트 되도록

--- src/main/java/com/ll/exam/app10/app/security/dto/MemberContext.java
+++ src/main/java/com/ll/exam/app10/app/security/dto/MemberContext.java
@@ -9,6 +9,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Getter
@@ -47,6 +48,10 @@ public class MemberContext extends User implements OAuth2User {
     @Override
     public Map<String, Object> getAttributes() {
         return this.attributes;
+    }
+
+    public String getProfileImgRedirectUrl() {
+        return "/member/profile/img/" + getId() + "?random=" + UUID.randomUUID();
     }
 
 }
