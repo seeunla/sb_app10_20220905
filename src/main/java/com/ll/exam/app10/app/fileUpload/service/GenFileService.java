229f7f695a6413b38f24e1f9227c341f3919004e
seeunla <107592967+seeunla@users.noreply.github.com>
2022-09-20 14:31:55 +0900

    파일 추가작업

--- src/main/java/com/ll/exam/app10/app/fileUpload/service/GenFileService.java
+++ src/main/java/com/ll/exam/app10/app/fileUpload/service/GenFileService.java
@@ -1,6 +1,8 @@
 package com.ll.exam.app10.app.fileUpload.service;
 
 import com.ll.exam.app10.app.article.entity.Article;
+import com.ll.exam.app10.app.base.AppConfig;
+import com.ll.exam.app10.app.base.dto.RsData;
 import com.ll.exam.app10.app.fileUpload.entity.GenFile;
 import com.ll.exam.app10.app.fileUpload.repository.GenfileRepository;
 import com.ll.exam.app10.util.Util;
@@ -8,18 +10,28 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class GenFileService {
     private final GenfileRepository genFileRepository;
-    public void saveFiles(Article article, Map<String, MultipartFile> fileMap) {
+    public RsData<Map<String, GenFile>> saveFiles(Article article, Map<String, MultipartFile> fileMap) {
         String relTypeCode = "article";
         long relId = article.getId();
 
+        Map<String, GenFile> genFileIds = new HashMap<>();
+
         for ( String inputName: fileMap.keySet()) {
             MultipartFile multipartFile = fileMap.get(inputName);
+
+            if (multipartFile.isEmpty()) {
+                continue;
+            }
+
 
             String[] inputNameBits = inputName.split("__");
 
@@ -49,6 +61,21 @@ public class GenFileService {
                     .build();
 
             genFileRepository.save(genFile);
+
+            String filePath = AppConfig.GET_FILE_DIR_PATH + "/" + fileDir + "/" + genFile.getFileName();
+
+            File file = new File(filePath);
+
+            file.getParentFile().mkdirs();
+
+            try {
+                multipartFile.transferTo(file);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            genFileIds.put(inputName, genFile);
         }
+        return new RsData("S-1", "파일을 업로드했습니다.", genFileIds);
     }
 }
